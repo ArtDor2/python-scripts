3f6cf717ef1209f59c6d35fe5a96d7a160356531
Arthur James Dorovskikh <19955193+ArtDor2@users.noreply.github.com>
2023-05-08 19:29:50 +0000

    Update rename-book.py
    can now recursively traverse files in subfolders, and has multiprocessing. Need to add to ignore files with extensions to next version

--- rename-book.py
+++ rename-book.py
@@ -1,12 +1,14 @@
-import magic # pip install python-magic
+import magic
 import os
 import shutil
+from concurrent.futures import ThreadPoolExecutor
+import threading
+
+processed_files_counter = 0
+counter_lock = threading.Lock()
 
 def add_extension(file_path):
-    # Get file type
     file_type = magic.from_file(file_path, mime=True)
-
-    # Map MIME types to extensions
     mime_to_ext = {
         'application/pdf': '.pdf',
         'application/epub+zip': '.epub',
@@ -19,34 +21,44 @@ def add_extension(file_path):
         'application/vnd.openxmlformats-officedocument.wordprocessingml.document': '.docx',
         'application/msword': '.doc',
         'application/x-chess-pgn': '.pgn',
-        'text/plain': '.txt'
+        'text/plain': '.txt',
+        'application/x-mobipocket-ebook': '.mobi'
     }
-
-    # Get the extension for the current file type
     extension = mime_to_ext.get(file_type)
-
     if extension:
-        # Add extension to the file
         new_file_path = file_path + extension
         return file_type, new_file_path
     else:
         return "Unsupported file type", None
 
+def process_file(file_path, files_to_rename):
+    global processed_files_counter
+    print(f"Processing file: {os.path.basename(file_path)}")
+    file_type, new_file_path = add_extension(file_path)
+    if new_file_path:
+        files_to_rename.append((file_path, new_file_path))
+
+    with counter_lock:
+        processed_files_counter += 1
+        print(f"Processed files: {processed_files_counter}")
+
 def process_directory(directory_path, files_to_rename):
-    for filename in os.listdir(directory_path):
-        file_path = os.path.join(directory_path, filename)
+    with ThreadPoolExecutor() as executor:
+        futures = []
+        for filename in os.listdir(directory_path):
+            file_path = os.path.join(directory_path, filename)
 
-        if os.path.isfile(file_path):
-            print(f"Processing file: {filename}")
-            file_type, new_file_path = add_extension(file_path)
+            if os.path.isfile(file_path):
+                futures.append(executor.submit(process_file, file_path, files_to_rename))
 
-            if new_file_path:
-                files_to_rename.append((file_path, new_file_path))
+            elif os.path.isdir(file_path):
+                process_directory(file_path, files_to_rename)
 
-        elif os.path.isdir(file_path):
-            process_directory(file_path, files_to_rename)
+        for future in futures:
+            future.result()
 
     return files_to_rename
+    
 
 def display_files_to_rename(files_to_rename):
     print("\nFiles to be renamed:")
@@ -58,7 +70,8 @@ def rename_files(files_to_rename):
         shutil.move(old_file_path, new_file_path)
         print(f"File renamed: {new_file_path}")
 
-directory_path = input("path/to/your/directory")
+# Get the directory path from the user
+directory_path = input("Enter the directory path: ")
 files_to_rename = []
 
 # Process the directory and get the list of files to rename
